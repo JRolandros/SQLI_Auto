4d2c732f969c4e06501d09bd13df8ad26f70b027
JRolandros <rolandakamba@yahoo.fr>
2015-12-25 22:56:12 +0100

    architecture step 1

---
+++ SQLI_Auto/SQLI_Auto/ViewModels/RootMapViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLI_Auto.ViewModels
+{
+    public class RootMapViewModels
+    {
+
+
+        // Try another root for the my trip
+        public void ChangeRootMap()
+        {
+
+        }
+
+        //set parameter for the root looking for
+        public void SetRootParameters()
+        {
+
+        }
+
+        //get details about this current root map
+        public void ShowRootMapDetails()
+        {
+
+        }
+    }
+}
---
+++ SQLI_Auto/SQLI_Auto/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLI_Auto.ViewModels
+{
+    public class WelcomeViewModel
+    {
+        //find a root map
+        public void FindRootMap()
+        {
+
+        }
+
+        public void MyProfil()
+        {
+
+        }
+
+        public void BookingCars()
+        {
+
+        }
+        
+        public void GoToNDF()
+        {
+
+        }
+
+    }
+}
